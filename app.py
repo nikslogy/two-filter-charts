a476171ef41c5cb4a46b9e7ea61fc154c4df1ea3
vaibhav <vaibhavnagargoje2000@gmail.com>
2025-04-27 12:28:15 +0530

    updated name == main

--- app.py
+++ app.py
@@ -525,4 +525,5 @@ def download_chart_code():
     return jsonify({'success': False, 'error': str('')})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
